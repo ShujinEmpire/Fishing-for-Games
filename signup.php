452b019c87b9eb994dc9346f402c1a64a9920704
Gracie-Curiel <graciecuriel7@gmail.com>
2026-04-22 13:53:28 -0700

    still need to rename labels

--- signup.php
+++ signup.php
@@ -1,1 +1,36 @@
+<html>
+<body>
+    <?php include 'main.php';?>
+    <form>
+    <!--FNAME-->    
+    <div class="mb-3">
+        <label for="input" class="form-label">First Name</label>
+        <input type="email" class="form-control" id="input" aria-describedby="emailHelp">
+    </div>
 
+    <!--LNAME-->
+    <div class="mb-3">
+        <label for="input" class="form-label">Last Name</label>
+        <input type="email" class="form-control" id="input" aria-describedby="emailHelp">
+    </div>
+
+    <!--Phone number-->
+    <div class="mb-3">
+        <label for="input" class="form-label">Phone Number</label>
+        <input type="email" class="form-control" id="input" aria-describedby="emailHelp">
+    </div>
+    
+    <!--EMAIL-->    
+    <div class="mb-3">
+        <label for="input" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="input" aria-describedby="emailHelp">
+    </div>
+    <!--PASSWORD-->
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</body>
+</html>
