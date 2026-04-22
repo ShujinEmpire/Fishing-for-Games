1babb884ae49d0a95944065e88f60228fba6c02d
Gracie-Curiel <graciecuriel7@gmail.com>
2026-04-22 13:54:25 -0700

    lol

--- login.php
+++ login.php
@@ -3,12 +3,12 @@
     <?php include 'main.php';?>
     <form>
     <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <label for="input" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="input" aria-describedby="emailHelp">
     </div>
     <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <label for="input" class="form-label">Password</label>
+        <input type="password" class="form-control" id="input">
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
     </form>
