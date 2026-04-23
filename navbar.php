4ff3b69990b627199b3a1bb6bf558d6d0e0a2f42
Gracie-Curiel <graciecuriel7@gmail.com>
2026-04-23 14:46:04 -0700

    navbar

---
+++ navbar.php
@@ -0,0 +1,17 @@
+<html>
+    <?php include 'main.php';?>
+    <ul class="nav justify-content-end">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Games</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Reviews</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Awards</a>
+        </li>
+    </ul>
+</html>
